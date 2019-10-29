5c0d733952929fdb7f23345e2608168ddb166de7
Colin Haney <ctimhaney@gmail.com>
2019-10-28 23:37:23 -0500

    Simple web site, no TLS

---
+++ src/main/java/com/ctimhaney/dev/web/HaneyDevWeb.java
@@ -0,0 +1,11 @@
+package com.ctimhaney.dev.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaneyDevWeb {
+  public static void main(String[] args) {
+    SpringApplication.run(HaneyDevWeb.class, args);
+  }
+}
